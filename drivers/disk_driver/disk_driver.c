d726a4c338be2cd561c8dc1d20685755a5158ddc
chaihomack <ivanlamash07@gmail.com>
2025-06-29 15:26:34 +0300

    better disk driver

--- drivers/disk_driver/disk_driver.c
+++ drivers/disk_driver/disk_driver.c
@@ -12,11 +12,20 @@
 #define COMMAND         0x1F7
 #define STATUS          0x1F7
 
+#define DRIVE_MASTER_CHS 0xA0
+#define DRIVE_SLAVE_CHS  0xB0
+#define DRIVE_MASTER_LBA 0xE0
+#define DRIVE_SLAVE_LBA  0xF0
+
+
 #define BSY   0x80
 #define DRQ   0x08
 #define ERR   0x01
 
-#define IDENTIFY_COMMAND 0xEC
+#define IDENTIFY_COMMAND    0xEC
+#define READ_COMMAND        0x20
+#define WRITE_COMMAND       0x30
+#define FLUSH_COMMAND       0xE7
 
 BYTE inb(WORD port)
 {
@@ -61,6 +70,32 @@ void outw(WORD port, WORD data)
     );
 }
 
+int wait_BSY_off()
+{
+    int timeout = 1000000;
+    while ((inb(STATUS) & BSY) && --timeout);          // waiting BSY=0
+    if(inb(STATUS) & ERR) return 1;
+    if (timeout == 0) return -1;        //time out
+    return 0;
+}
+
+int wait_DRQ_on()
+{
+    int timeout = 1000000;
+    while (!(inb(STATUS) & DRQ) && --timeout);      // waiting DRQ=1
+    if(inb(STATUS) & ERR) return 1;
+    if (timeout == 0) return -1;        //time out
+    return 0;
+}
+
+int wait_BSY_off_DRQ_on()
+{
+    int timeout = 1000000;
+    while (((inb(STATUS) & (BSY | DRQ)) != DRQ) && --timeout);   // wait for BSY=0 and DRQ=1
+    if(inb(STATUS) & ERR) return 1;
+    if (timeout == 0) return -1;        //time out
+    return 0;
+}
 
 BYTE ATA_disk_status()
 {
@@ -71,15 +106,14 @@ BYTE ATA_disk_init()
 {
 	outb(0x3F6, 0x04);  
     outb(0x3F6, 0x00);                  //resetting
-    while (inb(STATUS) & BSY);          // waiting for BSY=0
-    outb(DRIVE, 0xE0 | 0xA0);           // LBA + Master
+    wait_BSY_off();
+    outb(DRIVE, DRIVE_MASTER_LBA | DRIVE_MASTER_CHS);           // LBA + Master
 
-    outb(COMMAND, 0xEC);                // Identify
-    while (!(inb(STATUS) & 0x08));      // waiting DRQ=1
+    outb(COMMAND, IDENTIFY_COMMAND);
 
-    for (int i = 0; i < 256; i++) {
-        WORD tmp = inw(DATA);
-        (void)tmp;                  //reading output... but ignoring
+    for (int i = 0; i < 512; i++) {
+        BYTE tmp = inb(DATA);
+        (void)tmp;                 //reading output... but ignoring
     }
 
 	return 0;
@@ -87,7 +121,7 @@ BYTE ATA_disk_init()
 
 int ATA_disk_read(BYTE *buff, uint32_t sector, uint32_t count) 
 {
-    outb(DRIVE, 0xE0 | ((sector >> 24) & 0x0F)); // master and LBA
+    outb(DRIVE, DRIVE_MASTER_LBA | ((sector >> 24) & 0x0F)); // master and LBA
     
     outb(SECTOR_COUNT, count); 
     
@@ -95,21 +129,13 @@ int ATA_disk_read(BYTE *buff, uint32_t sector, uint32_t count)
     outb(LBA_MID, (sector >> 8) & 0xFF);   
     outb(LBA_HIGH, (sector >> 16) & 0xFF); 
     
-    outb(COMMAND, 0x20); //read command
+    outb(COMMAND, READ_COMMAND); 
     
-    while (1) 
-	{
-        BYTE status = inb(STATUS);
-
-        if (status & 0x01) return 1;      // ERR=1
-        if (!(status & 0x80) && (status & 0x08)) break; // BSY=0 and DRQ=1
-    }
-	
-    for (int i = 0; i < 256 * count; i++) 
-	{
-        *((WORD*)buff) = inw(DATA);
-        buff += 2;  
-    }
+    int rc = wait_BSY_off_DRQ_on();
+    if (rc) return rc;
+    
+    for (int i = 0; i < 512 * count; i++)
+        buff[i] = inb(DATA);
     
     return 0; 
 }
@@ -118,31 +144,21 @@ int ATA_disk_write(const BYTE *buff, uint32_t sector, uint32_t count)
 {
     if (!buff || count == 0) return -1;
 
-    outb(DRIVE, 0xE0 | ((sector >> 24) & 0x0F));  // LBA + Master
+    outb(DRIVE, DRIVE_MASTER_LBA | ((sector >> 24) & 0x0F));  // LBA + Master
 
     outb(SECTOR_COUNT, count);
     outb(LBA_LOW, sector);
     outb(LBA_MID, sector >> 8);
     outb(LBA_HIGH, sector >> 16);
 
-    outb(COMMAND, 0x30);  // write command
+    outb(COMMAND, WRITE_COMMAND);  // write command
 
-    // timeout
-    int timeout = 1000000;
-    while (((inb(STATUS) & (BSY | DRQ)) != DRQ) && --timeout);
-    if (timeout == 0) return -1;
-
-    const WORD *wbuff = (const WORD*) buff;
-    for (uint32_t i = 0; i < 256 * count; i++) {
-        outw(DATA, wbuff[i]);
+    for (uint32_t i = 0; i < 512 * count; i++) {
+        outb(DATA, buff[i]);
     }
 
-    timeout = 1000000;
-    while ((inb(STATUS) & BSY) && --timeout);
-    if (timeout == 0) return -1;
-
-    if (inb(STATUS) & ERR)
-        return -1;
+    int rc = wait_BSY_off();
+    if (rc) return rc;
 
     return 0;
 }
@@ -150,9 +166,9 @@ int ATA_disk_write(const BYTE *buff, uint32_t sector, uint32_t count)
 
 int ATA_disk_flush() 
 {    
-    outb(DRIVE, 0xE0 | 0xA0);  // LBA + Master
+    outb(DRIVE, DRIVE_MASTER_LBA | DRIVE_MASTER_CHS);  // LBA + Master
     
-    outb(COMMAND, 0xE7);  // flush command
+    outb(COMMAND, FLUSH_COMMAND);  // flush command
     
     DWORD timeout = 1000000;  //timeout
     while (--timeout) {
@@ -176,7 +192,8 @@ WORD ATA_get_word_from_DISK_IDENTIFY(uint32_t word_number)
 {
     outb(COMMAND, IDENTIFY_COMMAND);
     
-    while ((inb(STATUS) & (BSY | DRQ)) != DRQ); //waiting for BSY = 0 and DRQ = 1
+    int rc = wait_BSY_off_DRQ_on();
+    if (rc) return rc;
 
     WORD buffer[256];
 
@@ -188,7 +205,7 @@ WORD ATA_get_word_from_DISK_IDENTIFY(uint32_t word_number)
     return buffer[word_number];
 }
 
-unsigned int ATA_get_sector_count()
+uint32_t ATA_get_sector_count()
 {
     WORD lo = ATA_get_word_from_DISK_IDENTIFY(60);
     WORD hi = ATA_get_word_from_DISK_IDENTIFY(61);
